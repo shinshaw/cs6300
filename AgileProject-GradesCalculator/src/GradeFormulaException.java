806b12654ce0313054f1fed3e6b5634a695e531f
shinshaw <shinshaw86@gmail.com>
2016-08-18 16:36:32 -0500

    Delete GradeFormulaException.java

--- AgileProject-GradesCalculator/src/GradeFormulaException.java
+++
@@ -1,9 +0,0 @@
-package edu.gatech.seclass.gradescalculator;
-
-public class GradeFormulaException extends RuntimeException {
-
-	public GradeFormulaException(String message) {
-        super(message);
-	}
-
-}
